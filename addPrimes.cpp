139cd221ae86ffc70bf30ee642b1fb663a168862
Chris O'Neil <cconeil5@gmail.com>
2013-07-12 01:00:40 -0700

    added command line control

--- addPrimes.cpp
+++ addPrimes.cpp
@@ -1,16 +1,17 @@
+#include <cstdlib>
 #include <iostream>
 #include <iomanip>
 #include <vector>
 #include <cmath>
+#include <string>
 using namespace std;
 
-#define kPrintPrimes 0
 
 // this function calculates the sum of all prime numbers
 // up to the threshold.  This assumes that the threshold
 // is greater than 2, since two is the first true prime 
 // number. This function is Amortized O(n)
-void getPrimes(long long threshold, vector<long long> &primes);
+void getPrimes(const long long threshold, vector<long long> &primes);
 
 // this function takes in a vector of integers and returns
 // a sum of all of the integers.
@@ -21,22 +22,81 @@ long long sum2(vector<long long> &primes);
 void printPrimes(vector<long long> &primes);
 
 
-int main(void) {
-    long long threshold = 2000000;
+typedef struct argumentsStruct {
+    bool verbose = false;
+    bool showPrimes = false;
+    long long threshold;
+} argumentsType;
+
+// gets command line arguments
+argumentsType getArguments(int argc, char *argv[]);
+
+// checks to see if argument is a number
+bool isNumber(const string &s);
+
+
+int main(int argc, char *argv[]) {
+    argumentsType args = getArguments(argc, argv);
     vector<long long> primes;
+    long long threshold = args.threshold;
+    cout << "the threshold that you have entered is " << args.threshold << endl;
     getPrimes(threshold, primes);
-    if (kPrintPrimes) {
+    if (args.showPrimes) {
         printPrimes(primes);
     }
-    cout << "There are " << primes.size();
-    cout << " prime numbers between 2 and " << threshold << endl;
-    cout << "The smallest prime between 2 and " << threshold  << " is ";
-    cout << primes.front() << endl;
-    cout << "The largest prime between 2 and " << threshold << " is ";
-    cout << primes.back() << endl;
-    cout << "The sum of all the primes less than 2,000,000 is ";
+    if (args.verbose) {
+        cout << "There are " << primes.size();
+        cout << " prime numbers between 2 and " << threshold << endl;
+        cout << "The smallest prime between 2 and " << threshold  << " is ";
+        cout << primes.front() << endl;
+        cout << "The largest prime between 2 and " << threshold << " is ";
+        cout << primes.back() << endl;
+    }
+
+    cout << "The sum of all the primes less than " << threshold << " is ";
     cout << sum2(primes) << endl;
     return 0;
+}
+
+bool isNumber(const string &s)
+{
+    string::const_iterator it = s.begin();
+    while (it != s.end() && isdigit(*it)) {
+        ++it;
+    } 
+    return !s.empty() && it == s.end();
+}
+
+argumentsType getArguments(int argc, char *argv[]) {
+    argumentsType data;
+    if (argc < 2) {
+        cerr << "Please enter a threshold argument." << endl;
+        exit(1);
+    } else if (!isNumber(argv[1])) {
+        cerr << "Please enter a valid threshold as an argument" << endl;
+        exit(1);
+    }
+    data.threshold = atol(argv[1]);
+    for (int i = 2; i < argc; i++) {
+        string arg(argv[i]);
+        if (arg.compare("--verbose") == 0 || arg.compare("-v") == 0) {
+            data.verbose = true;
+        }
+        if (arg.compare("--showPrimes") == 0 || arg.compare("-s") == 0) {
+            data.showPrimes = true;
+        }
+        if (arg.compare("--help") == 0 || arg.compare("-h") == 0) {
+            cout << "This function takes a mandatory number as an argument ";
+            cout << "and returns all of the primes below that threshold added ";
+            cout << "together.\n  Specifying --verbose or -v will give more ";
+            cout << "information about the primes, such as largest, smallest ";
+            cout << "and how many primes.\n Speficifying --showPrimes or -s ";
+            cout << "will print an array of all of the prime numbers.\n";
+            cout << "Speficify --help or -h for help.\n";
+            exit(0);
+        }
+    }
+    return data;
 }
 
 void printPrimes(vector<long long> &primes) {
